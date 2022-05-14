a2b69776199174204cfdcda2c9eb26d80e2aa965
itsjustyagmur <44810927+itsjustyagmur@users.noreply.github.com>
2022-05-13 21:43:00 -0500

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+function copyEmail(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+  function copyNumber(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+  
